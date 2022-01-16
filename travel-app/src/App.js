a71ea6da1712b13f9b531456b617584d5b886f48
Gina Cooper <65682994+gina-cooper@users.noreply.github.com>
2022-01-16 15:01:41 -0500

    Update App.js

--- travel-app/src/App.js
+++ travel-app/src/App.js
@@ -7,10 +7,6 @@ import AddLocation from "./components/add-location.component";
 import EditLocation from "./components/edit-location.component";
 import CreateUser from "./components/create-user.component";
 
-// function GetID() {
-//     let {id} = useParams();
-//     return <{id}/>;
-// }
 
 function App() {
   return (
@@ -18,10 +14,7 @@ function App() {
       <div className = "container">
       <Navbar />
       <Routes>
-        {/* <Route path = "/" exact component={<ListLocations />} > </Route>
-        <Route path = "/add"  component = {AddLocation } />
-          <Route path = "/edit/:id"  component = { EditLocation } />
-          <Route path = "/user"  component = {CreateUser } /> */}
+
           <Route path = "/"  element = {<ListLocations />} ></Route> 
           <Route path = "/add"  element = {<AddLocation />} ></Route>
           <Route path = "/edit/:id"  element = {< EditLocation />} ></Route>
@@ -58,28 +51,3 @@ export default App;
 
 
 
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
